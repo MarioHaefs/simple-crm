7a1f3bfb9dde185ff989f451e623922f6668f325
Mario Haefs <MarioHaefs@t-online.de>
2023-09-07 14:44:28 +0200

    connected user class to mat-dialog

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models/user.class';
+import { MatDialogRef } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-dialog-add-user',
   templateUrl: './dialog-add-user.component.html',
   styleUrls: ['./dialog-add-user.component.scss']
 })
-export class DialogAddUserComponent {
 
-  onNoClick() {
+
+export class DialogAddUserComponent {
+  user = new User();
+  birthDate!: Date;
+
+  constructor(private dialogRef: MatDialogRef<DialogAddUserComponent>) {}
+
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log(this.user);
     
   }
 

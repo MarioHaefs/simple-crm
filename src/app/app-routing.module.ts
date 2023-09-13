98b4562207246aa53ca772a3ef009d5283f45c9d
Mario Haefs <MarioHaefs@t-online.de>
2023-09-13 11:43:59 +0200

    added info component incl content & design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { InfoComponent } from './info/info.component';
 
 const routes: Routes = [
   {path: '', component: DashboardComponent },
   {path: 'dashboard', component: DashboardComponent },
   {path: 'user', component: UserComponent },
-  {path: 'user/:id', component: UserDetailComponent }
+  {path: 'user/:id', component: UserDetailComponent },
+  {path: 'info', component: InfoComponent }
 ];
 
 @NgModule({

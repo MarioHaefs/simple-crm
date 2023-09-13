da91f47e2fd13dff5d3d67d770e55ea1c8a75bcf
Mario Haefs <MarioHaefs@t-online.de>
2023-09-13 10:28:28 +0200

    design & code improvements on user detail card

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,6 +1,7 @@
 export class User {
     firstName: string;
     lastName: string;
+    job: string;
     email: string;
     phone: string;
     address: string;
@@ -11,6 +12,7 @@ export class User {
     constructor(obj?: any) {
         this.firstName = obj ? obj.firstName : '';
         this.lastName = obj ? obj.lastName : '';
+        this.job = obj ? obj.job : '';
         this.email = obj ? obj.email : '';
         this.phone = obj ? obj.phone : '';
         this.address = obj ? obj.address : '';
@@ -24,6 +26,7 @@ export class User {
         return {
             firstName: this.firstName,
             lastName: this.lastName,
+            job: this.job,
             email: this.email,
             phone: this.phone,
             address: this.address,
